4388f031ce4755875f9d21c614fb50c9aca3a713
jaykantpandit <mr.jaykant@gmail.com>
2020-06-20 11:52:57 -0700

    scroll view dynamic height

---
+++ DynamicScrollView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicScrollView
+//
+//  Created by Jaykant on 21/06/20.
+//  Copyright © 2020 Jaykant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
